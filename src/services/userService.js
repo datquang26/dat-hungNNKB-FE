4846e0a24bb55bb467989a432fe59a52b2a7c082
duy <taquangdat2266@gmail.com>
2023-11-14 11:53:56 +0700

    vd42

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,21 @@ const handleLoginApi = (userEmail, userPassword) => {
     return axios.post('/api/login', { email: userEmail, password: userPassword });
 }
 
-export { handleLoginApi }
+const getAllUsers = (inputId) => {
+    return axios.get(`/api/get-all-users?id=${inputId}`)
+}
+
+const createNewUserService = (data) => {
+    console.log('check data from service: ', data)
+    return axios.post('/api/create-new-user', data)
+}
+
+const deteleUsersService = (userId) => {
+    return axios.delete('/api/delete-user', {
+        data: {
+            id: userId
+        }
+    })
+}
+
+export { handleLoginApi, getAllUsers, createNewUserService, deteleUsersService }
