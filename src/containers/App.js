614cc6a4e15d3f51618584757df9d80387520de2
duy <taquangdat2266@gmail.com>
2023-11-15 12:01:00 +0700

    vd49

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,6 +20,7 @@ import System from '../routes/System';
 import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
 import HomePage from './HomePage/HomePage.js';
+import CustomScrollbars from '../components/CustomScrollbars';
 
 class App extends Component {
 
@@ -50,12 +51,14 @@ class App extends Component {
                         {this.props.isLoggedIn && <Header />}
 
                         <span className="content-container">
+                            <CustomScrollbars style={{ height: '100vh', width: '100%'}}>
                             <Switch>
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
                                 <Route path={path.HOMEPAGE} component={HomePage} />
                             </Switch>
+                            </CustomScrollbars>
                         </span>
 
                         <ToastContainer
--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/logo.svg';
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from "../../utils";
+import { changeLanguageApp } from "../../store/actions"
 
 class HomeHeader extends Component {
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
 
     render() {
+        let language = this.props.language;
         
 
         return (
@@ -40,8 +46,18 @@ class HomeHeader extends Component {
                             <div className='support'><i className='fas fa-question-circle'></i>
                                 <FormattedMessage id="homeheader.support" />
                             </div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                                    VI
+                                </span>
+
+                            </div>
+                            <div  className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                                    EN
+                                </span>
+
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -99,6 +115,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
