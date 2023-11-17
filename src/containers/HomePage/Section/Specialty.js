0e6f51be953ed9e129ce62218647487a7876a64a
duy <taquangdat2266@gmail.com>
2023-11-17 08:51:28 +0700

    vd51

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -21,36 +21,36 @@ class Specialty extends Component {
         };
         
         return (
-            <div className='section-specialty'>
-                <div className='specialty-container'>
-                    <div className='specialty-header'>
+            <div className='section-share section-specialty'>
+                <div className='section-container'>
+                    <div className='section-header'>
                         <span className='title-section'>Chuyên khoa phổ biến</span>
                         <button className='btn-section'>Xem thêm</button>
                     </div>
-                    <div className='specialty-body'>
-                    <Slider {...setting}>
-                        <div className='specialty-customize'>
-                            <div className='bg-image'></div>
+                    <div className='section-body'>
+                    <Slider {...this.props.settings}>
+                        <div className='section-customize'>
+                            <div className='bg-image section-specialty'></div>
                             <div>Cở xương khớp 1</div>
                         </div>
-                        <div className='specialty-customize'>
-                            <div className='bg-image'></div>
+                        <div className='section-customize'>
+                            <div className='bg-image section-specialty'></div>
                             <div>Cở xương khớp 2</div>
                         </div>
-                        <div className='specialty-customize'>
-                            <div className='bg-image'></div>
+                        <div className='section-customize'>
+                            <div className='bg-image section-specialty'></div>
                             <div>Cở xương khớp 3</div>
                         </div>
-                        <div className='specialty-customize'>
-                            <div className='bg-image'></div>
+                        <div className='section-customize'>
+                            <div className='bg-image section-specialty'></div>
                             <div>Cở xương khớp 4</div>
                         </div>
-                        <div className='specialty-customize'>
-                            <div className='bg-image'></div>
+                        <div className='section-customize'>
+                            <div className='bg-image section-specialty'></div>
                             <div>Cở xương khớp 5</div>
                         </div>
-                        <div className='specialty-customize'>
-                            <div className='bg-image'></div>
+                        <div className='section-customize'>
+                            <div className='bg-image section-specialty'></div>
                             <div>Cở xương khớp 6</div>
                         </div>
                         
